055419aa8b8fdf4001ca6d5f1f7db10f932271e1
lxuxer42 <lxuxer42@gmail.com>
2023-09-03 22:36:14 +0200

    primeros archivos

---
+++ Makefile
@@ -0,0 +1,39 @@
+
+
+
+# --- Colors ---
+
+BOLD      := \033[1m
+BLACK     := \033[30;1m
+RED       := \033[31;1m
+GREEN     := \033[32;1m
+YELLOW    := \033[33;1m
+BLUE      := \033[34;1m
+MAGENTA   := \033[35;1m
+CYAN      := \033[36;1m
+WHITE     := \033[37;1m
+RESET     := \033[0m
+
+NAME_C		:= client
+NAME_S  	:= server
+
+CC			:= gcc
+
+CFLAGS		:= -g -Wall -Wextra -Werror
+
+# --- Library ---
+
+LIB			:= ar rcs
+
+LIBFT_DIR	:= ./libft
+LIBFT_PATH	:= %(LIBFT_DIR)/libft.a
+
+# --- Files ---
+
+SRCS		:= client.c server.c 
+
+# --- Rules ---
+
+$(OBJS)		:= obj/%.o : src/%.c 
+	@$(CC) $(CFLAGS) -c $< -o $@
+
---
+++ libft/ft_strcspn.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcspn.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 08:50:29 by rdelicad          #+#    #+#             */
+/*   Updated: 2023/08/23 08:55:02 by rdelicad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		while (reject[j] != '\0')
+		{
+			if (reject[j] == s[i])
+				return (i);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (i);
+}
