3414718ec2bd67aaaf7cabed6cf27d344e6dfa06
Ruben Delicado Garcia-Villaraco <rdelicad@c1r9s1.42malaga.com>
2023-09-08 18:12:39 +0200

    corregir el mutex y el server

--- src/client.c
+++ src/client.c
@@ -6,41 +6,64 @@
 /*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 16:32:53 by rdelicad          #+#    #+#             */
-/*   Updated: 2023/09/07 22:42:20 by rdelicad         ###   ########.fr       */
+/*   Updated: 2023/09/08 18:10:26 by rdelicad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
-static void    handler(int pid, const char *msn)
-{
-    int     i;
-    int     codigo;
-    char    caracter;
-    
+int	g_end_msn = 0;
 
-    i = 0;
-    while (msn[i] != '\0')
-    {
-        caracter = msn[i];
-        codigo = (int)caracter;
-        kill(pid, SIGUSR2 + codigo);
-        usleep(100);
-    }
+static void	sigusr_handler(int g_end_msn)
+{
+	if (g_end_msn == 1)
+		exit (0);
 }
 
-int main(int ac, char **av)
+static void	send_bits(int pid, char c)
 {
-    int         pid;
-    const char  *msn;
+	int	bit;
+	int	i;
 
-    pid = ft_atoi(av[1]);
-    msn = av[2];    
-    if (ac != 3)
-    {
-        ft_printf("Uso: %s <PID del servidor> <mensaje>\n", av[0]);
-        return (1);
-    }
-    handler(pid, msn);
-    return 0;
+	i = 7;
+	while (i > 0)
+	{
+		bit = (c >> i) & 1;
+		if (bit == 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(42);
+		i--;
+	}
+	if (i == 0)
+		sigusr_handler(1);
+}
+
+static void	handler(int pid, const char *msn)
+{
+	int	i;
+
+	i = 0;
+	while (msn[i] != '\0')
+	{
+		send_bits(pid, msn[i]);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int			pid;
+	const char	*msn;
+
+	pid = ft_atoi(av[1]);
+	msn = av[2];
+	if (ac != 3)
+	{
+		ft_printf("Uso: %s <PID del servidor> <mensaje>\n", av[0]);
+		return (1);
+	}
+	handler(pid, msn);
+	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,24 +6,32 @@
 /*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 15:15:01 by rdelicad          #+#    #+#             */
-/*   Updated: 2023/09/07 22:49:25 by rdelicad         ###   ########.fr       */
+/*   Updated: 2023/09/08 18:10:05 by rdelicad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
-static void	sigusr1_handler(int codigo_caracter)
+static void	sig_handler(int sig)
 {
-	char	caracter;
+	static char	c;
+	static int	bit_cont;
 
-	caracter = (char)codigo_caracter;
-	printf("Mensaje recibido del cliente: %c\n", caracter);
-	usleep(100);
+	if (sig == SIGUSR1)
+		c = c | (1 << (7 - bit_cont));
+	bit_cont++;
+	if (bit_cont == 8)
+	{
+		printf("Mensaje recibido del cliente: %c\n", c);
+		c = 0;
+		bit_cont = 0;
+	}
 }
 
-int main()
+int	main(void)
 {
-	signal(SIGUSR1, sigusr1_handler);
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
 	ft_printf("Servidor en ejecucion (PID: %d)\n", getpid());
 	while (1)
 	{
