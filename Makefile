ae00c4db4d5721aaddc40ef885909a52e910d92b
lxuxer42 <lxuxer42@gmail.com>
2023-09-10 16:04:03 +0200

    arreglando makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,4 @@
 
-
-
 # --- Colors ---
 
 BOLD      := \033[1m
@@ -26,7 +24,9 @@ CFLAGS		:= -g -Wall -Wextra -Werror
 LIB			:= ar rcs
 
 LIBFT_DIR	:= ./libft
-LIBFT_PATH	:= %(LIBFT_DIR)/libft.a
+LIBFT_PATH	:= $(LIBFT_DIR)/libft.a
+
+LIB_SYS		:= -Iinclude
 
 # --- Files ---
 
@@ -36,8 +36,38 @@ OBJS		:= $(addprefix obj/,$(SRCS:.c=.o))
 
 # --- Rules ---
 
-$
+$(NAME_C) : $(OBJS) $(LIBFT_PATH)
+	@echo "$(YELLOW)$(BOLD)Compiling Client...$(RESET)"
+	@$(CC) $(CFLAGS) $(LIBFT_PATH) $(LIB_SYS) -o $(NAME_C) 
+	@echo "$(GREEN)$(BOLD)Done.$(RESET)"
 
-$(OBJS)		:= obj/%.o : src/%.c 
+$(NAME_S) : $(OBJS) $(LIBFT_PATH)
+	@echo "$(YELLOW)$(BOLD)Compiling Server...$(RESET)"
+	@$(CC) $(CFLAGS) $(LIBFT_PATH) $(LIB_SYS) -o $(NAME_S) 
+	@echo "$(GREEN)$(BOLD)Done.$(RESET)"
+
+$(OBJS) : obj/%.o : src/%.c 
 	@$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFT_PATH) :
+	@make -s -C $(LIBFT_DIR)
+
+all : $(NAME_C) $(NAME_S)
+
+clean :
+	@echo "$(RED)$(BOLD)Cleaning objects from program...$(RESET)"
+	@rm -f $(OBJS)
+	@echo "$(GREEN)$(BOLD)Done.$(RESET)"
+	@make clean -s -C $(LIBFT_DIR)
+
+fclean :
+	@echo "$(RED)$(BOLD)Cleaning all files from program...$(RESET)"
+	@rm -f $(NAME_C) $(NAME_S) $(LIBFT_PATH) $(OBJS)
+	@make fclean -s -C $(LIBFT_DIR)
+
+re : fclean all
+
+minitalk : all clean
+
+.PHONY : all re fclean clean
 
--- src/client.c
+++ src/client.c
@@ -5,77 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/07 16:32:53 by rdelicad          #+#    #+#             */
-/*   Updated: 2023/09/09 17:19:50 by rdelicad         ###   ########.fr       */
+/*   Created: 2023/09/09 19:35:07 by rdelicad          #+#    #+#             */
+/*   Updated: 2023/09/10 15:19:23 by rdelicad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
-int	g_stop = 0;
-
-static void	send_bits(int pid, unsigned char c)
+void	handler(int pid, char *msn)
 {
-	int	bit;
-	int	i;
-
-	i = 7;
-	while (i >= 0)
+	int				i;
+	
+	while (*msn != '\0')
 	{
-		g_stop = 1;
-		bit = (c >> i) & 1;
-		if (bit == 1)
+		i = 7;
+		while (i >= 0)
 		{
-			if (kill(pid, SIGUSR1) == -1)
-				ft_error(2, "SIGURSR1 bad pid\n");
+			if (*msn & (1 << i))
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(3);
+			i--;
 		}
-		else if (bit == 0)
-		{
-			if (kill(pid, SIGUSR2) == -1)
-				ft_error(2, "SIGUSR2 bad pid\n");
-		}
-		while (g_stop == 0)
-			usleep(1000);
-		i--;
+		msn++;
 	}
-}
-
-static void	handler(int pid, const char *msn)
-{
-	int	i;
-
-	i = 0;
-	while (msn[i] != '\0')
-	{
-		send_bits(pid, msn[i]);
-		i++;
-	}
-	//send_bits(pid, '\0');
-}
-
-void	success(int sig)
-{
-	(void)sig;
-	g_stop = 1;
-	ft_printf("Los datos has sido recibidos.\n");
 }
 
 int	main(int ac, char **av)
 {
-	int			pid;
-	const char	*msn;
-
 	if (ac != 3 || !ft_str_isdigit(av[1]))
 	{
 		ft_printf("Uso: %s <PID del servidor> <mensaje>\n", av[0]);
 		return (1);
 	}
-	msn = av[2];
-	pid = ft_atoi(av[1]);
-	if (pid <= 0)
-		ft_error(2, "bad pid\n");
-	signal(SIGUSR1, success);
-	signal(SIGUSR2, success);
-	handler(pid, msn);
+	handler(ft_atoi(av[1]), av[2]);
 	return (0);
 }
